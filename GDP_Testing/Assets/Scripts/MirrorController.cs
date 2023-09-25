bf6fe1ec7c222a563c3b413df65ea730eacd2db3
Jonathan Jusup <jonathanjusup@gmail.com>
2023-09-25 14:42:50 +0200

    [ADDED] Laser Source Model + [MODIFIED] better mirror script
    Mirror rotates with clamped angles

--- GDP_Testing/Assets/Scripts/MirrorController.cs
+++ GDP_Testing/Assets/Scripts/MirrorController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,34 +13,57 @@ public class MirrorController : MonoBehaviour
     [SerializeField] private float lowerBound;
     [SerializeField] private float upperBound;
 
+    private Quaternion m_InitialRotation;
+    private float m_CurrentRotation = 0.0f;
+
+    private void Start()
+    {
+        m_InitialRotation = transform.rotation;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentRotation = transform.rotation.eulerAngles;
-        float rotationFactor = 0.0f;
+        CalcMirrorRotation();
+    }
 
+    private void CalcMirrorRotation()
+    {
+        float rotationFactor = 0.0f;
+        
         if (leftRotationTrigger && rightRotationTrigger)
         {
             if (leftRotationTrigger.isPressed && !rightRotationTrigger.isPressed)
             {
                 //Rotate CounterClockwise
                 Debug.Log("LEFT BUTTON PRESSED");
-                rotationFactor = -1.0f;
+                rotationFactor = 1.0f;
             } 
             else if (!leftRotationTrigger.isPressed && rightRotationTrigger.isPressed)
             {
                 //Rotate Clockwise
                 Debug.Log("RIGHT BUTTON PRESSED");
+                rotationFactor = -1.0f;
+            }
+        } 
+        else if (leftRotationTrigger)
+        {
+            if (leftRotationTrigger.isPressed)
+            {
                 rotationFactor = 1.0f;
             }
-            else
+        } 
+        else if (rightRotationTrigger)
+        {
+            if (rightRotationTrigger.isPressed)
             {
-                Debug.Log("NO BUTTON PRESSED");
+                rotationFactor = -1.0f;
             }
         }
-
-        float currentRotationSpeed = rotationSpeed * rotationFactor;
-        float newRotation = currentRotation.z + currentRotationSpeed * Time.deltaTime;
-        transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, newRotation);
+        
+        
+        m_CurrentRotation += rotationFactor * rotationSpeed * Time.deltaTime;
+        m_CurrentRotation = Mathf.Clamp(m_CurrentRotation, lowerBound, upperBound);
+        transform.rotation = m_InitialRotation * Quaternion.Euler(0.0f, 0.0f, m_CurrentRotation);
     }
 }
