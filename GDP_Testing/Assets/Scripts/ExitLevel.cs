5ade43504e8191d5272d27210b05dd0d31d4a077
FloKern97 <80768129+FloKern97@users.noreply.github.com>
2023-09-28 22:57:28 +0200

    Übergang zwischen Leveln gemacht

--- GDP_Testing/Assets/Scripts/ExitLevel.cs
+++ GDP_Testing/Assets/Scripts/ExitLevel.cs
@@ -1,18 +1,35 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ExitLevel : MonoBehaviour
 {
+    [SerializeField] private Animator transitionAnim;
+    [SerializeField] private float transitionTime = 1f;
     private Door doorScript;
+    
     private void OnTriggerEnter(Collider other)
     {
+        // TODO Debug entfernen, sobald man sicher ist, dass alles läuft wie es soll
+        Debug.Log("VERLASSE LEVEL");
         doorScript = GameObject.Find("Door").GetComponent<Door>();
         if (other.gameObject.CompareTag("Player") && doorScript.GetIsOpen())
         {
-            SceneManager.LoadScene(1);
+            LoadNextLevel();
         }
     }
+
+    private void LoadNextLevel()
+    {
+        // TODO ES MUSS EIGENTLICH +1 GERECHNET WERDEN, ÄNDERN, SOBALD TÜR AUCH IM INTRO IST
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        transitionAnim.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(index);
+    }
+    
 }
