013a73116fd0c55b2b4297f012815ab13cc75d7a
Jonathan Jusup <jonathanjusup@gmail.com>
2024-03-27 19:00:42 +0100

    [MODIFIED] Trigger.cs Documentation

--- RoboEscape/Assets/Scripts/Trigger.cs
+++ RoboEscape/Assets/Scripts/Trigger.cs
@@ -1,38 +1,35 @@
 using UnityEngine;
 
 /**
- * Class for the button inside the game. Controls the events that occur, when a button has been pressed.
+ * Class for the button inside the game. Controls the events that occur, when a button has
+ * been pressed.
  *
- * @authors Jonathan Jusup (cgt104707), Florian Kern (cgt104661)
+ * @authors Jonathan El Jusup (cgt104707), Florian Kern (cgt104661)
  */
-
 public class Trigger : TriggerInterface {
-    
-    /** Animator for the button */
+    /// <summary>
+    /// Button animator
+    /// </summary>
     private Animator _buttonAnimator;
     
-    /**
-     * Method is called before the first frame update.
-     * Sets the soundmanager in order to play a sound when the button is pressed and sets the controller for the
-     * cables that surround the button.
-     */
+    
+    /// <summary>
+    /// Called before the first frame update. Sets SoundManager in order to play a sound when
+    /// the button is pressed and sets the controller for the cables that connects the button.
+    /// </summary>
     private void Start() {
         _buttonAnimator = this.GetComponentInChildren<Animator>();
         SoundManager = FindObjectOfType<SoundManager>();
         CableController = GetComponent<CableController>();
     }
-    
-    /**
-     * Gets called when an object collides with the button.
-     * Updates the state of cables/activates them and triggers an animation that indicates that the button has been
-     * pressed.
-     *
-     * @param other The colliding object
-     */
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!isActivated)
-        {
+
+    /// <summary>
+    /// Called when an object collides with the button. Updates the state of cables/activates
+    /// them and triggers an animation that indicates that the button has been pressed.
+    /// </summary>
+    /// <param name="other">Other colliding object</param>
+    private void OnTriggerEnter(Collider other) {
+        if (!isActivated) {
             isActivated = true;
             // Activating cables
             CableController.UpdateState(isActivated);
@@ -46,18 +43,14 @@ public class Trigger : TriggerInterface {
             }
         }
     }
-    
-    /**
-     * Gets called when an object stays on top of the button.
-     * Updates the state of cables/activates them and triggers an animation that indicates that the button has been
-     * pressed.
-     *
-     * @param other The colliding object
-     */
-    private void OnTriggerStay(Collider other)
-    {
-        if (!isActivated)
-        {
+
+    /// <summary>
+    /// Called when an object stays on top of the button. Updates the state of cables/activates
+    /// them and triggers an animation that indicates that the button has been pressed.
+    /// </summary>
+    /// <param name="other">Other colliding object</param>
+    private void OnTriggerStay(Collider other) {
+        if (!isActivated) {
             isActivated = true;
             // Activating cables
             CableController.UpdateState(isActivated);
@@ -67,25 +60,20 @@ public class Trigger : TriggerInterface {
         }
     }
 
-    /**
-     * Gets called when an object exits the collision with the button.
-     * Deactivates the cables and triggers an animation to revert the button to its original position.
-     *
-     * @param other The colliding object
-     */
-    private void OnTriggerExit(Collider other)
-    {
-        if (isActivated)
-        {
+    /// <summary>
+    /// Called when an object exits the collision with the button. Deactivates the cables
+    /// and triggers an animation to revert the button to its original position.
+    /// </summary>
+    /// <param name="other">Other colliding object</param>
+    private void OnTriggerExit(Collider other) {
+        if (isActivated) {
             isActivated = false;
             // Deactivating cables
             CableController.UpdateState(isActivated);
         }
+
         // (Re)setting the triggers of the animation
         _buttonAnimator.ResetTrigger("Down");
         _buttonAnimator.SetTrigger("Up");
-
     }
-
-
 }
